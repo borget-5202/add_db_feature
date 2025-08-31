2633071d8ae7737d3357d6f99692b14e888c0115
borget-5202 <borgetteach@gmail.com>
2025-08-31 14:06:11 -0400

    Add files via upload

---
+++ database_add/blueprints/main.py
@@ -0,0 +1,12 @@
+# blueprints/main.py
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def homepage():
+    return render_template('index.html')
+
+@main_bp.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
