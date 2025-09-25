f92cdf2f73752359bd62d672510d721cea5be817
borget-5202 <borgetteach@gmail.com>
2025-09-25 06:32:52 -0400

    add game24 10,36,custom choice

--- db_features/config.py
+++ db_features/config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///arcade.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
 
     # Password reset
     SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT", "dev-salt-change-me")
@@ -15,3 +16,6 @@ class Config:
     REMEMBER_COOKIE_SECURE = True
     
     RATELIMIT_DEFAULT = "200 per hour; 50 per minute"
+
+    TARGETS_ENABLED = [24, 10, 36]
+    SOLVABLE_ONLY = False  # you can turn this on later
